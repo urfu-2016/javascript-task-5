236efa7fb73b21834908ed428140206fad96b051
askeip <paparazzi730@gmail.com>
2016-11-03 22:22:29 +0500

    no spacename

--- emitter.js
+++ emitter.js
@@ -4,14 +4,25 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-getEmitter.isStar = true;
+getEmitter.isStar = false;
 module.exports = getEmitter;
+
+function initEvent(students, miniEvent) {
+    for (var student in students) {
+        if (students.hasOwnProperty(student) &&
+            students[student].hasOwnProperty(miniEvent)) {
+            students[student][miniEvent]();
+        }
+    }
+}
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    var students = [];
+
     return {
 
         /**
@@ -19,26 +30,45 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object} this
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (students.indexOf(context) === -1) {
+                students.push(context);
+            }
+            context[event] = handler.bind(context);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} this
          */
         off: function (event, context) {
-            console.info(event, context);
+            delete context[event];
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} this
          */
         emit: function (event) {
-            console.info(event);
+            if (event === 'end') {
+                return this;
+            }
+            var splittedEvent = event.split('.');
+            for (var i = 0; i < splittedEvent.length; i++) {
+                var miniEvent = splittedEvent.slice(0, splittedEvent.length - i).join('.');
+                initEvent(students, miniEvent);
+            }
+
+            return this;
         },
 
         /**
