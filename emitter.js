306d2c8c4482ab97e2bccd8080dcd83788dc21a3
Koshcheev Nikita <nikitakosheleo@live.ru>
2016-11-04 03:01:01 +0500

    no star test pass

--- emitter.js
+++ emitter.js
@@ -4,14 +4,48 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-getEmitter.isStar = true;
+getEmitter.isStar = false;
 module.exports = getEmitter;
+
+function handle(eventList, event) {
+    console.info('<----------------New Event------------------>');
+    console.info(event);
+    console.info('<------------------Start-------------------->');
+
+    eventList.forEach(function (item) {
+        if (item.event.toString() === event.toString()) {
+            console.info('before');
+            console.info(item.context);
+
+            item.handler.call(item.context);
+
+            console.info('after');
+            console.info(item.context);
+            console.info('----------------------------------------------');
+        }
+    });
+    console.info('<----------------Event close------------------>');
+}
+
+function hasSubsting(target, string) {
+    var copy = target.slice();
+    while (copy.length) {
+        if (copy.toString() === string.toString()) {
+            return true;
+        }
+        copy.pop();
+    }
+
+    return false;
+}
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    var eventList = [];
+
     return {
 
         /**
@@ -19,26 +53,48 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
             console.info(event, context, handler);
+            eventList.push({
+                event: event.split('.'),
+                context: context,
+                handler: handler
+            });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
+            event = event.split('.');
             console.info(event, context);
+            eventList = eventList.filter(function (item) {
+                return item.context !== context || !hasSubsting(item.event, event);
+            });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            event = event.split('.');
+            while (event.length) {
+                handle(eventList, event);
+                event.pop();
+            }
+
+            return this;
         },
 
         /**
