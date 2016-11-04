f1f560af07b6300d3982764076c0f6cef633265e
Alex Solovyev <Alex Solovyev>
2016-11-04 22:56:31 +0500

    Without star

--- emitter.js
+++ emitter.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-getEmitter.isStar = true;
+getEmitter.isStar = false;
 module.exports = getEmitter;
 
 /**
@@ -14,31 +14,77 @@ module.exports = getEmitter;
 function getEmitter() {
     return {
 
+        eventsArray: {},
+
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!this.eventsArray.hasOwnProperty(event)) {
+                this.eventsArray[event] = [];
+            }
+
+            this.eventsArray[event].push({
+                'context': context,
+                'handler': handler
+            });
+
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            if (this.eventsArray.hasOwnProperty(event)) {
+                var signedPeople = this.eventsArray[event];
+                signedPeople
+                    .forEach(function (signedContext, index) {
+                        if (signedContext.context === context) {
+                            delete this.eventsArray[event][index];
+                        }
+                    }, this);
+            }
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            var splittedEvents = event.split('.');
+            var splittedEventsCount = splittedEvents.length;
+
+            splittedEvents
+                // Получаем список событий
+                .map(function (partOfEvent, index) {
+                    var eventsSlice = splittedEvents.slice(0, splittedEventsCount - index);
+
+                    return eventsSlice.join('.');
+                })
+                // Вызываем каждый событие
+                .forEach(function (splittedEvent) {
+                    if (this.eventsArray.hasOwnProperty(splittedEvent)) {
+
+                        this.eventsArray[splittedEvent]
+                            .forEach(function (contextObject) {
+                                contextObject.handler.call(contextObject.context);
+                            });
+                    }
+                }, this);
+
+            return this;
         },
 
         /**
@@ -48,9 +94,12 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object}
          */
         several: function (event, context, handler, times) {
             console.info(event, context, handler, times);
+
+            return this;
         },
 
         /**
@@ -60,9 +109,12 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object}
          */
         through: function (event, context, handler, frequency) {
             console.info(event, context, handler, frequency);
+
+            return this;
         }
     };
 }
