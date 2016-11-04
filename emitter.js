523cd2ed5f59259caf24a9601709522651334845
irina <irinasedova2011@gmail.com>
2016-11-04 22:46:59 +0500

    check task

--- emitter.js
+++ emitter.js
@@ -12,48 +12,89 @@ module.exports = getEmitter;
  * @returns {Object}
  */
 function getEmitter() {
+    var studentEvents = [];
+
     return {
 
-        /**
+        /*
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            // handler.call(context);
+            // console.info(event, context, handler);
+            studentEvents.push(createSubscription(event, context, handler));
+
+            return this;
         },
 
-        /**
+        /*
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
          */
         off: function (event, context) {
-            console.info(event, context);
+            // console.info(event, context);
+            studentEvents = studentEvents.slice().filter(function (element) {
+                var equalElement = element.nameEvent.indexOf(event) === -1;
+                equalElement = equalElement && element.nameEvent.indexOf(event + '.') === -1;
+                var equalContext = element.context !== context;
+
+                return equalElement || equalContext;
+            });
+
+            return this;
         },
 
-        /**
+        /*
          * Уведомить о событии
          * @param {String} event
          */
         emit: function (event) {
             console.info(event);
+            var divideEvents = event.split('.');
+            var fullEvent = event;
+            divideEvents.forEach(function () {
+                studentEvents.forEach(function (studentEvent) {
+                    if (studentEvent.nameEvent === fullEvent) {
+                        studentEvent.thisCount++;
+                        studentEvent.thisFriquency++;
+                        if (studentEvent.perform) {
+                            studentEvent.handler.call(studentEvent.context);
+                        }
+                    }
+                });
+
+                fullEvent = sliceEvent(fullEvent);
+            });
+
+            return this;
         },
 
-        /**
+        /*
          * Подписаться на событие с ограничением по количеству полученных уведомлений
          * @star
-         * @param {String} event
+         * @param { } event
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            // console.info(event, context, handler, times);
+            if (times < 1) {
+                this.on(event, context, handler);
+            } else {
+                var subscription = createSubscription(event, context, handler);
+                subscription.maxCount = times;
+                studentEvents.push(subscription);
+            }
+
+            return this;
         },
 
-        /**
+        /*
          * Подписаться на событие с ограничением по частоте получения уведомлений
          * @star
          * @param {String} event
@@ -62,7 +103,41 @@ function getEmitter() {
          * @param {Number} frequency – как часто уведомлять
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            // console.info(vent, context, handler, frequency);
+            if (frequency < 1) {
+                this.on(event, context, handler);
+            } else {
+                var subscription = createSubscription(event, context, handler);
+                subscription.mustFriquency = frequency;
+                studentEvents.push(subscription);
+            }
+
+            return this;
         }
     };
+}
+
+function createSubscription(event, context, handler) {
+    return {
+        get perform() {
+            var condition = (this.thisFriquency - 1) % this.mustFriquency === 0;
+            condition = condition && this.maxCount >= this.thisCount;
+
+            return condition;
+        },
+
+        thisFriquency: 0,
+        mustFriquency: 1,
+        thisCount: 0,
+        maxCount: Number.MAX_VALUE,
+        nameEvent: event,
+        context: context,
+        handler: handler
+    };
+}
+
+function sliceEvent(event) {
+    var spliteEvents = event.split('.');
+
+    return spliteEvents.slice(0, spliteEvents.length - 1).join('.');
 }
