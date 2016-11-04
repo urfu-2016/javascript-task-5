228928442807e51d951f10a1c24e490d554652d5
Zeace <pern2500@ya.ru>
2016-11-04 21:59:53 +0500

    правка + чистка

--- emitter.js
+++ emitter.js
@@ -16,67 +16,46 @@ function getEmitter() {
 
     return {
 
-
         on: function (event, context, handler) {
-
             if (events.hasOwnProperty(event)) {
                 events[event].push({ context: context, handler: handler });
             } else {
                 events[event] = [{ context: context, handler: handler }];
             }
 
-
             return this;
         },
 
-
         off: function (event, context) {
-            // console.log(events['slide.funny'])
             Object.keys(events).forEach(function (key) {
-
-                // console.log(events.key)
-               // console.log('a')
                 if (event.indexOf('.') > 0) {
-
-                    // console.log(events)
                     if (key === event) {
-                        // console.log(events[key])
-                        // console.log(events.key)
                         events[key] = events[key].filter(function (student) {
-
                             return student.context !== context;
                         });
-                        // console.log(events[key])
                     }
-
                 }
                 if (key.indexOf(event) >= 0) {
                     events[key] = events[key].filter(function (student) {
                         return student.context !== context;
                     });
                 }
-
             });
 
             return this;
         },
 
         emit: function (event) {
-            // console.log(events.begin)
-            // console.log(events.slide)
-            // console.log(events.slide.funny)
             startEvents(event, events);
             while (event.indexOf('.') !== -1) {
                 event = event.split('.').slice(0, -1)
-                    .join();
+                    .join('.');
                 startEvents(event, events);
             }
 
-
             return this;
         },
-
-
+        
         several: function (event, context, handler, times) {
             console.info(event, context, handler, times);
         },
