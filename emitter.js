a32a1d9af3df03028c693d44f3d920b51b011576
Gallyam Biktahsev <gallyamb@gmail.com>
2016-11-05 22:54:51 +0500

    Implement task

--- emitter.js
+++ emitter.js
@@ -12,6 +12,8 @@ module.exports = getEmitter;
  * @returns {Object}
  */
 function getEmitter() {
+    var events = {};
+
     return {
 
         /**
@@ -19,26 +21,75 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            var boundHandler = handler.bind(context);
+            boundHandler.boundThis = context;
+            Object.assign(boundHandler, handler);
+
+            if (!events[event]) {
+                events[event] = {
+                    handlers: []
+                };
+            }
+            events[event].handlers.push(boundHandler);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            Object.keys(events)
+                .filter(function (key) {
+                    return key.indexOf(event) === 0 &&
+                        ['', '.'].indexOf(key.substring(event.length, event.length)) === 0;
+                })
+                .forEach(function (key) {
+                    var handlers = events[key].handlers;
+                    for (var i = 0; i < handlers.length; i++) {
+                        if (handlers[i].boundThis === context) {
+                            handlers.splice(i, 1);
+                        }
+                    }
+                });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            while (event) {
+                if (events[event]) {
+                    events[event].handlers
+                        .filter(function (handler) {
+                            return (isNaN(handler.maxTimes) || handler.maxTimes > 0) &&
+                                (isNaN(handler.frequency) ||
+                                handler.frequency === handler.currentFrequency);
+                        })
+                        .forEach(function (handler) {
+                            handler();
+                            handler.maxTimes--;
+                            handler.currentFrequency = -1;
+                        });
+                    events[event].handlers
+                        .forEach(function (handler) {
+                            handler.currentFrequency++;
+                        });
+                }
+                event = event.substring(0, event.lastIndexOf('.'));
+            }
+
+            return this;
         },
 
         /**
@@ -48,9 +99,12 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object}
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            handler.maxTimes = times <= 0 ? NaN : times;
+
+            return this.on(event, context, handler);
         },
 
         /**
@@ -60,9 +114,13 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object}
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            handler.frequency = (frequency <= 0 ? undefined : frequency) - 1;
+            handler.currentFrequency = handler.frequency;
+
+            return this.on(event, context, handler);
         }
     };
 }
