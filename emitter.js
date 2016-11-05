3b0b54612f83b77a3e2b8fd431dceb3d9bd585ff
AtnesNess <atnesness@gmail.com>
2016-11-06 03:42:12 +0500

    First commit

--- emitter.js
+++ emitter.js
@@ -7,38 +7,216 @@
 getEmitter.isStar = true;
 module.exports = getEmitter;
 
+var LectureEvent = function (name, object, func) {
+    this._name = name;
+    this._subEvents = {};
+    if (func) {
+        this._function = func.bind(object);
+    }
+    this._object = object;
+    this._counter = undefined;
+    this._isSeveral = false;
+    this._isThrough = false;
+    this._freq = undefined;
+};
+
+Object.defineProperties(LectureEvent.prototype, {
+
+    _call: {
+        value: function (func, object) {
+            if (this._counter === undefined) {
+                func(object);
+
+                return;
+            }
+            if (this._isSeveral && this._counter > 0) {
+                func(object);
+
+                this._counter -= 1;
+            }
+            if (this._isThrough) {
+                if (this._counter === 0) {
+                    func(object);
+                    this._counter = this._freq - 1;
+                } else {
+                    this._counter -= 1;
+                }
+
+            }
+
+        }
+    },
+    emit: {
+        value: function () {
+
+            if (this._function) {
+                this._call(this._function, this._object);
+            }
+            if (this._parent) {
+                this._parent.emit();
+            }
+        }
+    },
+
+    setSeveral: {
+        value: function (times) {
+            this._isSeveral = true;
+            this._counter = times;
+        }
+    },
+
+    setThrough: {
+        value: function (freq) {
+            this._isThrough = true;
+            this._counter = 0;
+            this._freq = freq;
+        }
+    },
+
+    addSubEvent: {
+        value: function (event) {
+            if (event._object !== this._object && this._object !== undefined) {
+
+                return;
+            }
+            var splitted = event.name.split('.');
+            if (splitted.length > 1) {
+                event._name = splitted.slice(1, splitted.length).join('.');
+                var subEvents = this._subEvents[splitted[0]];
+                subEvents.forEach(function (subEvent) {
+                    subEvent.addSubEvent(event);
+                });
+
+            } else {
+                if (!(event.name in this._subEvents)) {
+                    this._subEvents[event.name] = [];
+                }
+                this._subEvents[event.name].push(event);
+                event.parent = this;
+            }
+        }
+    },
+
+    removeEvent: {
+        value: function (name, object) {
+            if (object !== this._object && this._object !== undefined) {
+
+                return;
+            }
+            var splitted = name.split('.');
+            if (splitted.length > 1) {
+                name = splitted.slice(1, splitted.length).join('.');
+                var subEvents = this._subEvents[splitted[0]];
+                subEvents.forEach(function (subEvent) {
+                    subEvent.removeEvent(name, object);
+                });
+            } else {
+                this._subEvents[name].forEach(function (subEvent) {
+                    if (subEvent._object === object) {
+                        subEvent._function = undefined;
+                    }
+                });
+            }
+        }
+    },
+    name: {
+        get: function () {
+
+            return this._name;
+        }
+    },
+
+    parent: {
+        get: function () {
+
+            return this._parent;
+        },
+
+        set: function (value) {
+            this._parent = value;
+
+            return this;
+        }
+    },
+
+    object: {
+        get function() {
+
+            return this._object;
+        }
+    }
+
+});
+
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+
     return {
+        _eventHandler: new LectureEvent('root'),
+        _events: {},
+        _addEvent: function (event, context, handler) {
+            var lectEvent = new LectureEvent(event, context, handler);
+            this._eventHandler.addSubEvent(lectEvent);
+            if (!(event in this._events)) {
+                this._events[event] = [];
+            }
+            this._events[event].push(lectEvent);
+
+            return lectEvent;
+        },
 
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Emmitter} this
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            this._addEvent(event, context, handler);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Emmitter} this
          */
         off: function (event, context) {
-            console.info(event, context);
+            this._eventHandler.removeEvent(event, context);
+            this._events[event] = this._events[event].filter(function (ev) {
+
+                return context !== ev.object;
+            });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Emmitter} this
          */
         emit: function (event) {
-            console.info(event);
+            while (!(event in this._events) && event.split('.').length > 1) {
+                event = event.split('.').slice(0, -1)
+                .join('.');
+            }
+            if (!(event in this._events)) {
+
+                return this;
+            }
+
+            this._events[event].forEach(function (lectEvent) {
+                lectEvent.emit();
+            });
+
+            return this;
         },
 
         /**
@@ -48,9 +226,13 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Emmitter} this
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            var lesEvent = this._addEvent(event, context, handler);
+            lesEvent.setSeveral(times);
+
+            return this;
         },
 
         /**
@@ -60,9 +242,13 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Emmitter} this
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            var lesEvent = this._addEvent(event, context, handler, 'through');
+            lesEvent.setThrough(frequency);
+
+            return this;
         }
     };
 }
