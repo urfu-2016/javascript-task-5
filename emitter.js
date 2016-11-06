2964fefaeac34632fd5546edce1c275672f854f3
AtnesNess <atnesness@gmail.com>
2016-11-06 14:51:32 +0500

    Rebuilding the idea 	Making only one chain

--- emitter.js
+++ emitter.js
@@ -7,85 +7,95 @@
 getEmitter.isStar = true;
 module.exports = getEmitter;
 
-var LectureEvent = function (name, object, func) {
+var LectureEvent = function (name) {
     this._name = name;
     this._subEvents = {};
-    if (func) {
-        this._function = func.bind(object);
-    }
-    this._object = object;
-    this._counter = undefined;
-    this._isSeveral = false;
-    this._isThrough = false;
-    this._freq = undefined;
-    this._parents = [];
+    this._handlers = [];
+    this._parent = undefined;
 };
 
 Object.defineProperties(LectureEvent.prototype, {
 
     _call: {
-        value: function (func, object) {
-            if (this._counter === undefined) {
-                func(object);
+        value: function (handler) {
+            var func = handler.function;
+            var context = handler.context;
+            if (handler.counter === undefined) {
+                func(context);
 
                 return;
             }
-            if (this._isSeveral && this._counter > 0) {
-                func(object);
-                this._counter -= 1;
+            if (handler.isSeveral && handler.counter > 0) {
+                func(context);
+                handler.counter -= 1;
             }
-            if (this._isThrough) {
-                if (this._counter === 0) {
-                    func(object);
-                    this._counter = this._freq - 1;
+            if (handler.isThrough) {
+                if (handler.counter === 0) {
+                    func(context);
+                    handler.counter = handler.freq - 1;
                 } else {
-                    this._counter -= 1;
+                    handler.counter -= 1;
                 }
             }
         }
     },
     emit: {
         value: function () {
-
-            if (this._function) {
-                this._call(this._function, this._object);
+            this._handlers.forEach(function (handler) {
+                var func = handler.function;
+                if (func) {
+                    this._call(handler);
+                }
+            }.bind(this));
+            if (this._parent) {
+                this._parent.emit();
             }
-            var parents = this._parents.slice();
-            parents.reverse().forEach(function (parent) {
-                parent.emit();
-            });
         }
     },
-
     setSeveral: {
-        value: function (times) {
+        value: function (times, context) {
             if (times > 0) {
-                this._isSeveral = true;
-                this._counter = times;
+                this._handlers.forEach(function (handler) {
+                    if (handler.context !== context) {
+
+                        return;
+                    }
+                    handler.isSeveral = true;
+                    handler.counter = times;
+                });
             }
         }
     },
 
     setThrough: {
-        value: function (freq) {
+        value: function (freq, context) {
             if (freq > 0) {
-                this._isThrough = true;
-                this._counter = 0;
-                this._freq = freq;
+                this._handlers.forEach(function (handler) {
+                    if (handler.context !== context) {
+
+                        return;
+                    }
+                    handler.isThrough = true;
+                    handler.counter = 0;
+                    handler.freq = freq;
+                });
             }
+        }
+    },
+
+    addHandler: {
+        value: function (context, handler) {
+            this._handlers.push({ 'function': handler.bind(context),
+                                             'context': context });
         }
     },
 
     addSubEvent: {
         value: function (event) {
-            if (event._object !== this._object && this._object !== undefined) {
-
-                return;
-            }
             var splitted = event.name.split('.');
             if (splitted.length > 1) {
-                if (! this._subEvents[splitted[0]]) {
-                    var emptyEvent = new LectureEvent(splitted[0], this._object);
+                if (!this._subEvents[splitted[0]]) {
+                    var emptyEvent = new LectureEvent(splitted[0]);
                     this.addSubEvent(emptyEvent);
                 }
                 event._name = splitted.slice(1, splitted.length).join('.');
@@ -98,41 +108,39 @@ Object.defineProperties(LectureEvent.prototype, {
                     this._subEvents[event.name] = [];
                 }
                 this._subEvents[event.name].push(event);
-                event._parents.push(this);
+                event._parent = this;
             }
         }
     },
+
     _deepDisable: {
-        value: function (events) {
+        value: function (events, context) {
             for (var key in events) {
                 if (!events[key]) {
 
                     return;
                 }
                 events[key].forEach(function (event) {
-                    event._function = undefined;
-                    this._deepDisable(event._subEvents);
+                    this.removeEvent(event.name, context);
                 }.bind(this));
             }
         }
-
     },
     removeEvent: {
-        value: function (name, object) {
-            if (object !== this._object && this._object !== undefined) {
-
-                return;
-            }
+        value: function (name, context) {
             var splitted = name.split('.');
             if (splitted.length > 0 && splitted[0] !== '') {
                 name = splitted.slice(1, splitted.length).join('.');
                 var subEvents = this._subEvents[splitted[0]];
                 subEvents.forEach(function (subEvent) {
-                    subEvent.removeEvent(name, object);
+                    subEvent.removeEvent(name);
                 });
             } else {
-                this._function = undefined;
-                this._deepDisable(this._subEvents);
+                this._handlers = this._handlers.filter(function (handler) {
+
+                    return handler.context !== context;
+                });
+                this._deepDisable(this._subEvents, context);
             }
         }
     },
@@ -162,14 +170,14 @@ function getEmitter() {
         _eventHandler: new LectureEvent('root'),
         _events: {},
         _addEvent: function (event, context, handler) {
-            var lectEvent = new LectureEvent(event, context, handler);
-            this._eventHandler.addSubEvent(lectEvent);
             if (!(event in this._events)) {
-                this._events[event] = [];
+                var lectEvent = new LectureEvent(event);
+                this._eventHandler.addSubEvent(lectEvent);
+                this._events[event] = lectEvent;
             }
-            this._events[event].push(lectEvent);
+            this._events[event].addHandler(context, handler);
 
-            return lectEvent;
+            return this._events[event];
         },
 
         /**
@@ -192,11 +200,7 @@ function getEmitter() {
          * @returns {Emmitter} this
          */
         off: function (event, context) {
-            this._events[event] = this._events[event].filter(function (ev) {
-                ev.removeEvent('', context);
-
-                return context !== ev.object;
-            });
+            this._events[event].removeEvent('', context);
 
             return this;
         },
@@ -212,13 +216,9 @@ function getEmitter() {
                 .join('.');
             }
             if (!(event in this._events)) {
-
                 return this;
             }
-
-            this._events[event].forEach(function (lectEvent) {
-                lectEvent.emit();
-            });
+            this._events[event].emit();
 
             return this;
         },
@@ -234,7 +234,7 @@ function getEmitter() {
          */
         several: function (event, context, handler, times) {
             var lesEvent = this._addEvent(event, context, handler);
-            lesEvent.setSeveral(times);
+            lesEvent.setSeveral(times, context);
 
             return this;
         },
@@ -250,7 +250,7 @@ function getEmitter() {
          */
         through: function (event, context, handler, frequency) {
             var lesEvent = this._addEvent(event, context, handler, 'through');
-            lesEvent.setThrough(frequency);
+            lesEvent.setThrough(frequency, context);
 
             return this;
         }
