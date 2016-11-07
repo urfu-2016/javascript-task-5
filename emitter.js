ddaed6489c8c4425252a7ffa60ddadcb265e87a0
imatasov <ivan.matasov@yandex.ru>
2016-11-07 20:53:57 +0500

    first version

--- emitter.js
+++ emitter.js
@@ -4,8 +4,10 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-getEmitter.isStar = true;
+getEmitter.isStar = false;
 module.exports = getEmitter;
+
+var NAME_SPACE = {};
 
 /**
  * Возвращает новый emitter
@@ -19,26 +21,61 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {this}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!NAME_SPACE.hasOwnProperty(event)) {
+                NAME_SPACE[event] = [];
+            }
+
+            NAME_SPACE[event].push ({
+                context: context,
+                handler: handler
+            });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {this}
          */
         off: function (event, context) {
-            console.info(event, context);
+            Object.keys(NAME_SPACE).forEach(function () {
+                if (NAME_SPACE.hasOwnProperty(event)) {
+                    NAME_SPACE[event].map(function (record, index) {
+                        if (record.context === context) {
+                            delete NAME_SPACE[event][index];
+                        }
+
+                        return false;
+                    });
+                }
+            }, this);
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {this}
          */
         emit: function (event) {
-            console.info(event);
+            var arrayEvents = event.split('.');
+            var currentEvent;
+            for (var i = arrayEvents.length; i !== 0; i--) {
+                currentEvent = arrayEvents.slice(0, i).join('.');
+                if (NAME_SPACE.hasOwnProperty(currentEvent)) {
+                    NAME_SPACE[currentEvent].forEach(function (record) {
+                        record.handler.call(record.context);
+                    });
+                }
+            }
+
+            return this;
         },
 
         /**
