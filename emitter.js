0e41765389edd61254797ecc57181f2b59835baf
Dorokhova Tatiana <t.dorohovav@gmail.com>
2016-11-08 18:08:34 +0500

    задача без доп задания

--- emitter.js
+++ emitter.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-getEmitter.isStar = true;
+getEmitter.isStar = false;
 module.exports = getEmitter;
 
 /**
@@ -12,57 +12,72 @@ module.exports = getEmitter;
  * @returns {Object}
  */
 function getEmitter() {
+
     return {
+        events: [],
 
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * returns {Object}
          */
+
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            this.events.push(
+                {
+                    event: event,
+                    context: context,
+                    handler: handler
+                }
+            );
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * returns {Object}
          */
+
         off: function (event, context) {
-            console.info(event, context);
+            this.events = this.events.filter(function (currentEvent) {
+                if (currentEvent.event.indexOf(event + '.') === 0) {
+                    return currentEvent.context !== context;
+                }
+
+                return currentEvent.event !== event || currentEvent.context !== context;
+            });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * returns {Object}
          */
+
         emit: function (event) {
-            console.info(event);
-        },
+            var developments = event.split('.')
+            .reduce(function (acc, nameFunction) {
+                var newNameFunction = acc.length > 0 ? [acc, nameFunction].join('.') : nameFunction;
+                acc.push(newNameFunction);
 
-        /**
-         * Подписаться на событие с ограничением по количеству полученных уведомлений
-         * @star
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         * @param {Number} times – сколько раз получить уведомление
-         */
-        several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
-        },
+                return acc;
+            }, []);
+            developments.reverse().forEach(function (nameFunction) {
+                this.events.forEach(function (currentEvent) {
+                    if (currentEvent.event === nameFunction) {
+                        currentEvent.handler.call(currentEvent.context);
+                    }
+                });
+            }, this);
 
-        /**
-         * Подписаться на событие с ограничением по частоте получения уведомлений
-         * @star
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         * @param {Number} frequency – как часто уведомлять
-         */
-        through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            return this;
         }
     };
 }
