51c4267cd03cb17e29b92ab3d6542564f6e1a064
Трефилов Андрей <trefilovandew@yandex.ru>
2016-11-09 15:48:23 +0500

    Исправления

--- emitter.js
+++ emitter.js
@@ -12,16 +12,16 @@ module.exports = getEmitter;
  * @returns {Object}
  */
 function getEmitter() {
-    var subscriberEvents = {};
-    var count = 0;
+    var eventListeners = {};
+    var currentIssueEvent = 0;
 
-    function performEvents(event) {
-        var eventsKeys = Object.keys(subscriberEvents);
-        var check = eventsKeys.indexOf(event.split('.')[0]);
+    function runEventHandlers(event) {
+        var eventsKeys = Object.keys(eventListeners);
+        var eventIssue = eventsKeys.indexOf(event.split('.')[0]);
 
-        if (count - check > -1) {
-            count = check + 1;
-            subscriberEvents[event].forEach(function (subscriber) {
+        if (currentIssueEvent - eventIssue > -1) {
+            currentIssueEvent = eventIssue + 1;
+            eventListeners[event].forEach(function (subscriber) {
                 subscriber.handler.call(subscriber.context);
             });
         }
@@ -31,11 +31,11 @@ function getEmitter() {
 
         on: function (event, context, handler) {
             // console.info(event, context, handler);
-            if (!subscriberEvents.hasOwnProperty(event)) {
-                subscriberEvents[event] = [];
+            if (!eventListeners.hasOwnProperty(event)) {
+                eventListeners[event] = [];
             }
 
-            subscriberEvents[event].push({
+            eventListeners[event].push({
                 context: context,
                 handler: handler
             });
@@ -46,26 +46,23 @@ function getEmitter() {
         off: function (event, context) {
             // console.info(event, context);
 
-            if (subscriberEvents.hasOwnProperty(event)) {
-                var events = Object.keys(subscriberEvents);
-                var searchedEvents = [event];
+            if (eventListeners.hasOwnProperty(event)) {
+                var events = Object.keys(eventListeners);
 
+                eventListeners[event].splice(0, 1);
                 events.forEach(function (searchedEvent) {
-                    var check = searchedEvent.split('.');
-                    for (var i = 0; i < check.length; i++) {
-                        var nameEvent = check.slice(0, i).join('.');
-                        if (searchedEvent !== event &&
-                            nameEvent === event) {
-                            searchedEvents.push(searchedEvent);
+                    var checkEventNames = searchedEvent.split('.');
+                    for (var i = 0; i < checkEventNames.length; i++) {
+                        if (searchedEvent.indexOf(event) === 0) {
+                            eventListeners[searchedEvent].filter(function (subscriber, id) {
+                                if (subscriber.context === context) {
+                                    return eventListeners[searchedEvent].splice(id, 1);
+                                }
+
+                                return eventListeners[searchedEvent];
+                            });
                         }
                     }
-                });
-                searchedEvents.forEach(function (searchEvent) {
-                    subscriberEvents[searchEvent].forEach(function (subscriber, i) {
-                        if (subscriber.context === context) {
-                            subscriberEvents[searchEvent].splice(i, 1);
-                        }
-                    });
                 });
             }
 
@@ -78,8 +75,8 @@ function getEmitter() {
 
             for (var i = namesEvent.length; i > -1; i--) {
                 var nameEvent = namesEvent.slice(0, i).join('.');
-                if (subscriberEvents.hasOwnProperty(nameEvent)) {
-                    performEvents(nameEvent);
+                if (eventListeners.hasOwnProperty(nameEvent)) {
+                    runEventHandlers(nameEvent);
                 }
             }
 
