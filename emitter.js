551d40f561db6d51c4976c06e68af305017b677a
v0kuzyo <nikolayevvadim@gmail.com>
2016-11-09 22:51:51 +0500

    local test complete

--- emitter.js
+++ emitter.js
@@ -4,14 +4,17 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-getEmitter.isStar = true;
+getEmitter.isStar = false;
 module.exports = getEmitter;
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
+
 function getEmitter() {
+    var subs = [];
+
     return {
 
         /**
@@ -19,26 +22,55 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
             console.info(event, context, handler);
+            subs.push(getNewSubscription(event, context, handler));
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
             console.info(event, context);
+            subs = subs.filter(function (sub) {
+                var contIsCont = sub.context !== context;
+                var eventIsEvent = sub.event !== event;
+
+                return contIsCont || eventIsEvent;
+            });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
             console.info(event);
+            var doHandler = function (sub) {
+                if (sub.event === event) {
+                    sub.handler.call(sub.context);
+                }
+            };
+            var len = event.split('.').length;
+            for (var i = 0; i < len; i++) {
+                subs.forEach(doHandler);
+                event = event
+                    .split('.')
+                    .slice(0, -1)
+                    .join('.');
+            }
+
+            return this;
         },
 
         /**
@@ -64,5 +96,13 @@ function getEmitter() {
         through: function (event, context, handler, frequency) {
             console.info(event, context, handler, frequency);
         }
+    };
+}
+
+function getNewSubscription(event, context, handler) {
+    return {
+        event: event,
+        context: context,
+        handler: handler
     };
 }
