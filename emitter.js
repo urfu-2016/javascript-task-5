4a6b9d065c2c5f5fa193e046a2091f459ff8f68b
Mikhail Pliskovsky <snoward@yandex.ru>
2016-11-09 22:12:07 +0500

    Базовая задача

--- emitter.js
+++ emitter.js
@@ -4,15 +4,36 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-getEmitter.isStar = true;
+getEmitter.isStar = false;
 module.exports = getEmitter;
+
+function startsWith(prefix, string) {
+    return string.indexOf(prefix) !== -1;
+}
+
+function isUpperEvent(event) {
+    return event.indexOf('.') !== -1;
+}
+
+function getSubEvents(event) {
+    var result = [event];
+
+    while (isUpperEvent(event)) {
+        event = event.slice(0, event.lastIndexOf('.'));
+        result.push(event);
+    }
+
+    return result;
+}
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+
     return {
+        handlers: {},
 
         /**
          * Подписаться на событие
@@ -21,16 +42,41 @@ function getEmitter() {
          * @param {Function} handler
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!this.handlers[event]) {
+                this.handlers[event] = [];
+            }
+
+            this.handlers[event].push(
+                {
+                    context: context,
+                    handler: handler.bind(context)
+                }
+            );
+
+            return this;
         },
 
         /**
          * Отписаться от события
-         * @param {String} event
+         * @param {String} eventToOff
          * @param {Object} context
          */
-        off: function (event, context) {
-            console.info(event, context);
+        off: function (eventToOff, context) {
+            var events = Object.keys(this.handlers);
+
+            events = events.filter(function (event) {
+                var eventPrefix = event + '.';
+
+                return event === eventToOff || startsWith(eventPrefix, event);
+            });
+
+            events.forEach(function (element) {
+                events[element] = events[element].filter(function (event) {
+                    return event.context !== context;
+                });
+            });
+
+            return this;
         },
 
         /**
@@ -38,7 +84,16 @@ function getEmitter() {
          * @param {String} event
          */
         emit: function (event) {
-            console.info(event);
+            var schedule = this.handlers;
+
+            var eventsToEmit = getSubEvents(event);
+            eventsToEmit.forEach(function (eventToEmit) {
+                schedule[eventToEmit].forEach(function (subscriber) {
+                    subscriber.handler();
+                });
+            });
+
+            return this;
         },
 
         /**
