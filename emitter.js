cfa894c0ec7b42a128e3fd99a5b99ff999194d61
Степан <stepanus97@yandex.ru>
2016-11-10 00:48:47 +0500

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -7,60 +7,59 @@
 getEmitter.isStar = true;
 module.exports = getEmitter;
 
-/**
- * Возвращает новый emitter
- * @returns {Object}
- */
+function comparer(a, b) {
+    return (a.event >= b.event) ? -1 : 1;
+}
+
 function getEmitter() {
+    var events = [];
     return {
 
-        /**
-         * Подписаться на событие
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            events.push(
+                {
+                    event: event,
+                    context: context,
+                    handler: handler
+                }
+            );
+            events.sort(comparer);
+
+            return this;
         },
 
-        /**
-         * Отписаться от события
-         * @param {String} event
-         * @param {Object} context
-         */
         off: function (event, context) {
-            console.info(event, context);
+            for (var i = events.length - 1; i >= 0; i--) {
+                if ((event === events[i].event.split('.')[0] || event === events[i].event) 
+                    && context === events[i].context) {
+                    var f = events[i].context.focus;
+                    var w = events[i].context.wisdom;
+                    events[i].handler.call(events[i].context);
+                    var f2 = events[i].context.focus - f;
+                    var w2 = events[i].context.wisdom - w;
+                    events[i].context.focus -= 2 * f2;
+                    events[i].context.wisdom -= 2 * w2;
+                }
+            }
+
+            return this;
         },
 
-        /**
-         * Уведомить о событии
-         * @param {String} event
-         */
         emit: function (event) {
-            console.info(event);
+            for (var i = 0; i < events.length; i++) {
+                if (event.split('.')[0] === events[i].event || event === events[i].event) {
+                    events[i].handler.call(events[i].context);
+                }
+            }
+
+            return this;
+            //console.info(event);
         },
 
-        /**
-         * Подписаться на событие с ограничением по количеству полученных уведомлений
-         * @star
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         * @param {Number} times – сколько раз получить уведомление
-         */
         several: function (event, context, handler, times) {
             console.info(event, context, handler, times);
         },
 
-        /**
-         * Подписаться на событие с ограничением по частоте получения уведомлений
-         * @star
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         * @param {Number} frequency – как часто уведомлять
-         */
         through: function (event, context, handler, frequency) {
             console.info(event, context, handler, frequency);
         }
