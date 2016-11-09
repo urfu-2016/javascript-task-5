662431483ced2cdd363fb01d276ea9f37e5f02ab
AnastasiaBabushkina <nastena.babushkina.96@mail.ru>
2016-11-09 22:43:53 +0500

    версия 1

--- emitter.js
+++ emitter.js
@@ -7,38 +7,62 @@
 getEmitter.isStar = true;
 module.exports = getEmitter;
 
-/**
- * Возвращает новый emitter
- * @returns {Object}
- */
+function isExistParameters(event, context, student) {
+    if (student !== undefined) {
+        var regexp = new RegExp(event + '.');
+        var isExistElement = student.event === event;
+        var eventNames = student.event.match(regexp);
+        var isRootEventName = eventNames !== null ? eventNames.index === 0 : false;
+        var isExistStudent = student.parameters === context;
+
+        return isExistStudent || isRootEventName && isExistElement;
+    }
+
+    return false;
+}
+
 function getEmitter() {
+    var subscriptions = [];
+
     return {
 
-        /**
-         * Подписаться на событие
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            subscriptions.push({
+                parameters: context,
+                event: event,
+                func: handler
+            });
+
+            return this;
         },
 
-        /**
-         * Отписаться от события
-         * @param {String} event
-         * @param {Object} context
-         */
+
         off: function (event, context) {
-            console.info(event, context);
+            for (var i = subscriptions.length; i >= 0; i--) {
+                if (isExistParameters(event, context, subscriptions[i])) {
+                    subscriptions.splice(i, 1);
+                }
+            }
+
+            return this;
         },
 
-        /**
-         * Уведомить о событии
-         * @param {String} event
-         */
         emit: function (event) {
-            console.info(event);
+            var namespace = event;
+            event.split('.').forEach(function () {
+                subscriptions.forEach(function (student) {
+                    if (student.event === namespace) {
+                        student.func.call(student.parameters);
+                    }
+                });
+
+                namespace = namespace
+                    .split('.')
+                    .slice(0, -1)
+                    .join('.');
+            });
+
+            return this;
         },
 
         /**
