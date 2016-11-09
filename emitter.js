d53488722444b1ca23027ae3e4c2a4b0678a9328
Antonov Alexey <aleksei193@mail.ru>
2016-11-09 16:58:10 +0500

    Первое решение

--- emitter.js
+++ emitter.js
@@ -7,6 +7,40 @@
 getEmitter.isStar = true;
 module.exports = getEmitter;
 
+function changeEvent(string) {
+    var e = string.lastIndexOf('.');
+
+    return (e !== -1) ? string.slice(0, e) : '';
+}
+
+function findIndex(collection, functionI) {
+    for (var i = 0; i < collection.length; i++) {
+        if (functionI(collection[i])) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+function find(collection, functionI) {
+    var i = findIndex(collection, functionI);
+
+    return (i !== -1) ? collection[i] : undefined;
+}
+
+function deleteSubscriber(subscribers, subscriber) {
+    var index = findIndex(subscribers, function (item) {
+        return item.object === subscriber;
+    });
+
+    if (index !== -1) {
+        subscribers.splice(index, 1);
+    }
+}
+
+var SEPARATOR = '.';
+
 /**
  * Возвращает новый emitter
  * @returns {Object}
@@ -14,31 +48,85 @@ module.exports = getEmitter;
 function getEmitter() {
     return {
 
+        events: {},
+
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object} this
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!this.events.hasOwnProperty(event)) {
+                this.events[event] = [];
+            }
+
+            var subscribers = this.events[event];
+
+            var subscriber = find(subscribers, function (item) {
+                return item.object === context;
+            });
+
+            if (subscriber === undefined) {
+                subscriber = { object: context, actions: [] };
+                subscribers.push(subscriber);
+            }
+
+            subscriber.actions.push(handler);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} this
          */
         off: function (event, context) {
-            console.info(event, context);
+            var subscribers = this.events[event];
+            if (subscribers === undefined) {
+                return this;
+            }
+
+            deleteSubscriber(subscribers, context);
+
+            var previousEvent = event + SEPARATOR;
+
+            var previousEvents = Object.keys(this.events).filter(function (eventItem) {
+                return eventItem.indexOf(oldEvent) === 0;
+            });
+
+            previousEvents.forEach(function (eventItem) {
+                deleteSubscriber(this.events[eventItem], context);
+            }, this);
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} this
          */
         emit: function (event) {
-            console.info(event);
+            while (event !== '') {
+                var subscribers = this.events[event];
+                event = changeEvent(event);
+
+                if (subscribers === undefined) {
+                    continue;
+                }
+
+                subscribers.forEach(function (subscriber) {
+                    subscriber.actions.forEach(function (action) {
+                        action.call(subscriber.object);
+                    });
+                });
+            }
+
+            return this;
         },
 
         /**
@@ -48,9 +136,27 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object} this
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            if (times <= 0) {
+                return this.on(event, context, handler);
+            }
+
+            var countOfCall = 0;
+
+            /**
+            * @this {Object}
+            */
+            function wrapperOfHandler() {
+                if (countOfCall !== times) {
+                    handler.call(this);
+                    console.info(54, this);
+                    countOfCall++;
+                }
+            }
+
+            return this.on(event, context, wrapperOfHandler);
         },
 
         /**
@@ -60,9 +166,28 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object} this
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            if (frequency <= 0) {
+                return this.on(event, context, handler);
+            }
+
+            var countOfCall = 1;
+
+            /**
+            * @this {Object}
+            */
+            function wrapperOfHandler() {
+                if (countOfCall !== frequency) {
+                    handler.call(this);
+                    countOfCall++;
+                } else {
+                    countOfCall = 1;
+                }
+            }
+
+            return this.on(event, context, wrapperOfHandler);
         }
     };
 }
