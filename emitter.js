62b2a3e63b6324ea1f53c18adc992017f4213953
Chernykh Artyom <chernykhand@mail.ru>
2016-11-09 23:45:30 +0500

    first try

--- emitter.js
+++ emitter.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-getEmitter.isStar = true;
+getEmitter.isStar = false;
 module.exports = getEmitter;
 
 /**
@@ -14,31 +14,70 @@ module.exports = getEmitter;
 function getEmitter() {
     return {
 
+        listeners: [],
+        getEventsList: function (event) {
+            var events = event.split('.');
+            events = events.map(function (value, index) {
+                return events.slice(0, events.length - index).join('.');
+            });
+
+            return events;
+        },
+
+        getStartEvent: function (event, lengthEvent) {
+            return event.split('.').slice(0, lengthEvent).
+                        join('.');
+        },
+
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object} this
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+
+            this.listeners.push({ event: event, context: context, handler: handler });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} this
          */
         off: function (event, context) {
-            console.info(event, context);
+            var _this = this;
+            var lengthEvent = event.split('.').length;
+            this.listeners = this.listeners.filter(function (listener) {
+                return ((_this.getStartEvent(listener.event, lengthEvent) !== event) &&
+                        (listener.context !== context));
+            });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} this
          */
         emit: function (event) {
-            console.info(event);
+            var _this = this;
+            var events = this.getEventsList(event);
+            events.forEach(function (currentEvent) {
+                _this.listeners.forEach(function (listener) {
+                    if (listener.event === currentEvent) {
+                        listener.handler.call(listener.context);
+                    }
+                });
+            });
+
+
+            return this;
         },
 
         /**
