482f65491109f80e3ea3efa243850cdf74926af4
VeraTimox <v.timoxina123@yandex.ru>
2016-11-11 17:39:28 +0500

    названия переменных

--- emitter.js
+++ emitter.js
@@ -4,24 +4,24 @@ getEmitter.isStar = false;
 module.exports = getEmitter;
 
 function getEmitter() {
-    var subscribed = {};
+    var subscriptions = {};
 
     return {
         on: function (event, context, handler) {
-            if (!subscribed.hasOwnProperty(event)) {
-                subscribed[event] = [];
+            if (!subscriptions.hasOwnProperty(event)) {
+                subscriptions[event] = [];
             }
-            subscribed[event].push({ 'context': context, 'handler': handler });
+            subscriptions[event].push({ 'context': context, 'handler': handler });
 
             return this;
         },
 
         off: function (event, context) {
-            Object.keys(subscribed).forEach(function (key) {
+            Object.keys(subscriptions).forEach(function (key) {
                 if (event === key || event + '.' === key.substr(0, event.length + 1)) {
-                    subscribed[key].forEach(function (elem) {
+                    subscriptions[key].forEach(function (elem) {
                         if (elem.context === context) {
-                            subscribed[key].splice(subscribed[key].indexOf(elem), 1);
+                            subscriptions[key].splice(subscriptions[key].indexOf(elem), 1);
                         }
                     });
                 }
@@ -31,18 +31,18 @@ function getEmitter() {
         },
 
         emit: function (event) {
-            var allEvents = [];
+            var eventsToEmit = [];
             while (event.length !== 0) {
-                allEvents.push(event);
+                eventsToEmit.push(event);
                 if (event.lastIndexOf('.') !== -1) {
                     event = event.slice(0, event.lastIndexOf('.'));
                 } else {
                     event = '';
                 }
             }
-            allEvents.forEach(function (item) {
-                if (subscribed.hasOwnProperty(item)) {
-                    subscribed[item].forEach(function (elem) {
+            eventsToEmit.forEach(function (eventToEmit) {
+                if (subscriptions.hasOwnProperty(eventToEmit)) {
+                    subscriptions[eventToEmit].forEach(function (elem) {
                         elem.handler.call(elem.context);
                     });
                 }
