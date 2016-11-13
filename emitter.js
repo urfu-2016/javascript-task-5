7f09e472254260d35a9bbc6fb4c5285da5da811a
Tsvetkova Valeria <lera.tsvetkova96@gmail.com>
2016-11-13 16:55:47 +0500

    Решение

--- emitter.js
+++ emitter.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-getEmitter.isStar = true;
+getEmitter.isStar = false;
 module.exports = getEmitter;
 
 /**
@@ -12,33 +12,76 @@ module.exports = getEmitter;
  * @returns {Object}
  */
 function getEmitter() {
+
     return {
+
+        getChildrenEvents: function (event) {
+            var events = event.split('.');
+            for (var i = 1; i < events.length; i++) {
+                events[i] = events[i - 1].concat('.').concat(events[i]);
+            }
+
+            return events.reverse();
+        },
+
+        subscriptions: {},
 
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+
+            if (!this.subscriptions.hasOwnProperty(event)) {
+                this.subscriptions[event] = [];
+            }
+
+            this.subscriptions[event].push({
+                context: context,
+                handler: handler.bind(context)
+            });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            for (var subscription in this.subscriptions) {
+                if (this.subscriptions.hasOwnProperty(subscription) && (subscription === event ||
+                    subscription.indexOf(event.concat('.') !== -1))) {
+                    this.subscriptions[event] = this.subscriptions[event].filter(function (name) {
+                        return name.context !== context;
+                    });
+                }
+            }
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            var subscriptions = this.subscriptions;
+            (this.getChildrenEvents(event)).forEach(function (subscription) {
+                if (subscriptions.hasOwnProperty(subscription)) {
+                    subscriptions[subscription].forEach(function (element) {
+                        element.handler();
+                    });
+                }
+            });
+
+            return this;
         },
 
         /**
