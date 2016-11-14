8015b6bf9039a1eb0092f03ceca1df05a86a034f
Степан <stepanus97@yandex.ru>
2016-11-14 19:16:12 +0500

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -11,13 +11,17 @@ function comparer(a, b) {
     return (a.event >= b.event) ? -1 : 1;
 }
 
+/**
+ * Сравнивает два пространства имен, и проверяет, является ли более короткое частью более длинного
+ * @param {String} bigNamespace - "длинное" пространство имен
+ * @param {Object} smallNamespace - "короткое" пространство имен
+ * @returns {Boolean}
+ */
 function compareNamespaces(bigNamespace, smallNamespace) {
     var splitedBigNS = bigNamespace.split('.');
     var splitedSmallNS = smallNamespace.split('.');
     for (var i = 0; i < splitedSmallNS.length; i++) {
-        if (splitedSmallNS[i] === splitedBigNS[i]) {
-            continue;
-        } else {
+        if (splitedSmallNS[i] !== splitedBigNS[i]) {
             return false;
         }
     }
@@ -25,11 +29,21 @@ function compareNamespaces(bigNamespace, smallNamespace) {
     return true;
 }
 
+/**
+ * Возвращает новый emitter
+ * @returns {Object}
+ */
 function getEmitter() {
     var events = [];
 
     return {
 
+        /**
+         * Подписаться на событие
+         * @param {String} event
+         * @param {Object} context
+         * @param {Function} handler
+         */
         on: function (event, context, handler) {
             events.push(
                 {
@@ -43,16 +57,25 @@ function getEmitter() {
             return this;
         },
 
+        /**
+         * Отписаться от события
+         * @param {String} event
+         * @param {Object} context
+         */
         off: function (event, context) {
-            for (var i = 0; i < events.length; i++) {
-                if (compareNamespaces(events[i].event, event) && context === events[i].context) {
-                    delete events[i].handler;
+            events.forEach(function (item, i) {
+                if (compareNamespaces(item.event, event) && context === item.context) {
+                    delete item.handler;
                 }
-            }
+            });
 
             return this;
         },
 
+        /**
+         * Уведомить о событии
+         * @param {String} event
+         */
         emit: function (event) {
             for (var i = 0; i < events.length; i++) {
                 if (compareNamespaces(event, events[i].event) &&
@@ -64,10 +87,26 @@ function getEmitter() {
             return this;
         },
 
+        /**
+         * Подписаться на событие с ограничением по количеству полученных уведомлений
+         * @star
+         * @param {String} event
+         * @param {Object} context
+         * @param {Function} handler
+         * @param {Number} times – сколько раз получить уведомление
+         */
         several: function (event, context, handler, times) {
             console.info(event, context, handler, times);
         },
 
+        /**
+         * Подписаться на событие с ограничением по частоте получения уведомлений
+         * @star
+         * @param {String} event
+         * @param {Object} context
+         * @param {Function} handler
+         * @param {Number} frequency – как часто уведомлять
+         */
         through: function (event, context, handler, frequency) {
             console.info(event, context, handler, frequency);
         }
